48043f16cac06b8c22d4949f57a5fd170fb69521
Cliff Rodgers <frodgers@wisc.edu>
2015-03-13 22:36:33 -0500

    homework 1 plus hanoi!

--- cis194/homework-1/src/HW1/TowersOfHanoi.hs
+++ cis194/homework-1/src/HW1/TowersOfHanoi.hs
@@ -3,5 +3,15 @@ module HW1.TowersOfHanoi (hanoi) where
 type Peg = Char
 type Move = (Peg, Peg)
 
+moveFrom :: Peg -> Peg -> Move
+moveFrom a b = (a, b)
+
+moveSeq :: Integer -> Peg -> Peg -> [Move]
+moveSeq n a b
+  | n == 1 = [moveFrom a b]
+  | n > 1 = (moveFrom a b) : (moveSeq (n - 1) a b)
+
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi n a b c
+  | n == 0 = []
+  | otherwise = (moveSeq (n - 1) a c) ++ (moveSeq 1 a b) ++ (moveSeq (n - 1) c b)
