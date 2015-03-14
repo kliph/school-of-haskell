8b738797700d098b31a6db900001e96ae0ef0fdb
Cliff Rodgers <frodgers@wisc.edu>
2015-03-13 21:36:40 -0500

    homework 1 passes

--- cis194/homework-1/src/HW1/CreditCard.hs
+++ cis194/homework-1/src/HW1/CreditCard.hs
@@ -8,17 +8,40 @@ module HW1.CreditCard
 
 type CCNumber = Integer
 
+stringToInteger :: String -> Integer
+stringToInteger c = read c :: Integer
+
+splitString :: String -> [Integer]
+splitString [] = []
+splitString (c:rest) = stringToInteger([c]) : splitString(rest)
+
 toDigits :: CCNumber -> [Integer]
-toDigits = undefined
+toDigits n
+  | n <= 0 = []
+  | n > 0 = splitString(show n)
 
 toDigitsRev :: CCNumber -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n = reverse(toDigits(n))
+
+doubleFromLeft :: [Integer] -> [Integer]
+doubleFromLeft [] = []
+doubleFromLeft (x:[]) = [x]
+doubleFromLeft (x:(y:zs)) = x : 2 * y : doubleFromLeft zs
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . doubleFromLeft . reverse
+
+sumFlatDigits :: [Integer] -> Integer
+sumFlatDigits [] = 0
+sumFlatDigits(n:[]) = n
+sumFlatDigits(n:ns) = n + sumFlatDigits(ns)
+
+flattenDigitsAndSum :: Integer -> Integer
+flattenDigitsAndSum n = sumFlatDigits(toDigits(n))
 
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits [] = 0
+sumDigits(n:ns) = flattenDigitsAndSum(n) + sumDigits(ns)
 
 validate :: CCNumber -> Bool
-validate = undefined
+validate n = sumDigits(doubleEveryOther(toDigits(n))) `mod` 10 == 0
