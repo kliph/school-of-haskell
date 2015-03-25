9cae2a4f10e26262698330fa4b0d5822ec2212dd
Cliff Rodgers <frodgers@wisc.edu>
2015-03-25 12:58:44 -0500

    Starting over Revert "parses messages but can't handle unknown"
    This reverts commit 5794b12cb08d8447b0caa9110f3fd22269b06f33.

--- cis194/homework-2/src/HW2/LogAnalysis.hs
+++ cis194/homework-2/src/HW2/LogAnalysis.hs
@@ -32,45 +32,11 @@ brent = Person "Brent" 31 SealingWax
 baz :: Person -> String
 baz p@(Person n _ _) = "The Name field of (" ++ show p ++ ") is " ++ n
 
-parseError :: String -> MessageType
-parseError s = (Error (read (drop 1 (take 3 s)) :: Int))
-
-parseMessageType :: String -> MessageType
-parseMessageType s = case s of
-  ('W':s) -> Warning
-  ('E':s) -> parseError(s)
-  ('I':s) -> Info
-             -- parseMessageType (t:_:e:rest)
-             -- _ -> Unknown (e ++ rest)
-takeChar :: String -> Char
-takeChar(c:rest) = c
-
-takeUntilSpace :: String -> String
-takeUntilSpace s = case s of
-  (' ':s) -> []
-  _ -> takeChar(s) : takeUntilSpace(drop 1 s)
-
-parseTimeStamp :: String -> TimeStamp
-parseTimeStamp s
-  | (parseMessageType s) == Info = (read (takeUntilSpace (drop 2 s)) :: Int)
-  | (parseMessageType s) == Warning = (read (takeUntilSpace (drop 2 s)) :: Int)
-  | otherwise = (read (takeUntilSpace (drop 4 s)) :: Int)
-
-parseMessageString :: String -> String
-parseMessageString s
-  | (parseMessageType s) == Info = (drop (3 + (length (show (parseTimeStamp s)))) s)
-  | (parseMessageType s) == Warning = (drop (3 + (length (show (parseTimeStamp s)))) s)
-  | otherwise = (drop (5 + (length (show (parseTimeStamp s)))) s)
-
-
 parseMessage :: String -> LogMessage
-parseMessage s = LogMessage (parseMessageType s) (parseTimeStamp s) (parseMessageString s)
+parseMessage = undefined
 
 parse :: String -> [LogMessage]
-parse x = case x of
-  [] -> [Unknown "Foobar"]
-  ('E':s) -> [LogMessage (Error 2) 562 "help help"]
-  ('I':s) -> [LogMessage Info 29 "la la la"]
+parse = undefined
 
 insert :: LogMessage -> MessageTree -> MessageTree
 insert = undefined
