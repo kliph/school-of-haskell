8740d89046436360fcdfaa4b2017bea9edff5380
Cliff Rodgers <frodgers@wisc.edu>
2015-03-25 19:50:26 -0500

    parseMessage works

--- cis194/homework-2/src/HW2/LogAnalysis.hs
+++ cis194/homework-2/src/HW2/LogAnalysis.hs
@@ -33,7 +33,11 @@ baz :: Person -> String
 baz p@(Person n _ _) = "The Name field of (" ++ show p ++ ") is " ++ n
 
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage s = case (words s !! 0) of
+  ("I") -> LogMessage Info (read (words s !! 1) :: TimeStamp) (unwords (drop 2 (words s)))
+  ("W") -> LogMessage Warning (read (words s !! 1) :: TimeStamp) (unwords (drop 2 (words s)))
+  ("E") -> LogMessage (Error (read (words s !! 1) :: Int)) (read ((words s) !! 2) :: TimeStamp) (unwords (drop 3 (words s)))
+  _ -> Unknown s
 
 parse :: String -> [LogMessage]
 parse = undefined
